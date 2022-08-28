5b802474367607f4140d63cb7bd96607e9ce8341
Sharqawi-1995 <Sharqawi-1995@users.noreply.github.com>
2022-08-29 01:50:13 +0300

    on branch main

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,23 +3,25 @@ import React, { Component } from "react";
 export default class Transaction extends Component {
   deleteTrans = () => {
     this.props.deleteTransaction(this.props.transaction._id);
-  }
+  };
 
   render() {
     let transaction = this.props.transaction;
 
     return (
       <div className="Transaction">
-        <p>Category:{transaction.category}</p>
+        Category:
+        <p>{transaction.category}</p>
+        Amount:
         <p>
-          Amount:
           <span style={{ color: transaction.amount >= 0 ? "green" : "red" }}>
             {transaction.amount}
           </span>
         </p>
-        <p>vendor:{transaction.vendor}</p>
+        Vendor:
+        <p>{transaction.vendor}</p>
         <button onClick={this.deleteTrans}>Delete</button>
       </div>
-    )
+    );
   }
 }
